5b7ab4365b12b6f5b74fe7658cc08a79d316a9fd
Aman Khajuria <24khajuriaa@gmail.com>
2026-04-02 20:12:13 -0700

    Designing basic webpage structure and style Will try to finish at least 2 tabs of style on html, and finish the last one tomorrow I can then finally move the webpage structure to JS and start using working on/learning about E6 modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 import path from "node:path"
+import { fileURLToPath } from "node:url";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default {
   mode: "development",
   entry: "./src/index.js",
@@ -10,6 +15,9 @@ export default {
   },
   devtool: "eval-source-map",
   devServer: {
+    static: {
+      directory: path.join(__dirname, "images"),
+    },
     watchFiles: ["./src/template.html"],
     open: true,
   },
@@ -24,6 +32,10 @@ export default {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+      test: /\.(png|svg|jpe?g|gif)$/i,
+      type: "asset/resource",
       },
     ],
   },
