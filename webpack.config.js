e87f5aea714dc84c14efc97c1aa4d45566d66dd3
Aman Khajuria <24khajuriaa@gmail.com>
2026-04-06 18:40:49 -0700

    Accidentally changed the favicon path
    This was done as a mistake in the last commit. Figured out the error pretty fast

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ export default {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
-      favicon: "../images/beach.png",
+      favicon: "./images/beach.png",
     }),
   ],
   module: {
